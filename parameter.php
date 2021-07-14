e1c4b7274b46d3b331dabca5121f5b3136b62a14
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2021-07-14 17:31:41 +0900

    php_parameter

---
+++ parameter.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>안녕하세요. wooseobee님</h1>
+    <h1>안녕하세요. <?php echo $_GET['address'];?>에 사시는 <?php echo $_GET['name'];?>님</h1>
+  </body>
+</html>

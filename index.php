d6f7162bc047c563d4c57134bec49750a52493b5
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2021-07-14 21:14:41 +0900

    php 함수

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title></title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php?id=WEB">WEB</a></h1>
     <ol>
       <li><a href="index.php?id=HTML">HTML</a></li>
       <li><a href="index.php?id=CSS">CSS</a></li>
@@ -16,7 +16,9 @@
         echo $_GET['id'];
       ?>
     </h2>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+    <?php
+      $data = file_get_contents("data/".$_GET['id']);
+      echo $data;
+    ?>
   </body>
 </html>

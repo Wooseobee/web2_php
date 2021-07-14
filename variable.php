b395a898c26c25919caeffde8d42bb9638ce0772
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2021-07-14 17:12:10 +0900

    php 변수

---
+++ variable.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "Shin woo seob";
+    echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ".$name." et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco wooseob laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure wooseob dolor in reprehenderit in voluptate velit esse cillum ".$name." dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ".$name." sunt in culpa qui officia deserunt mollit anim wooseob id est laborum.<br/>\n";
+    ?>
+    <?php
+    $a = "변수1";
+    echo $a."+변수2";
+    ?>
+  </body>
+</html>

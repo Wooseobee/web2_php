f9d3cd7245fcf7ee0cc700b14c29bd9625f27429
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2021-07-16 14:54:18 +0900

    php form과  post(글 생성,수정,삭제)

--- create.php
+++ create.php
@@ -27,7 +27,7 @@ function print_list(){
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>
@@ -44,21 +44,16 @@ function print_list(){
       ?>
     </ol>
     <a href="create.php">create</a>
-    <?php if(isset($_GET['id'])) {?>
-    <a href="update.php?id=<?=$_GET['id']?>">update</a>
-    <form class="" action="delete_process.php" method="post">
-      <input type="hidden" name="id" value="<?=$_GET['id']?>">
-      <input type="submit" value="delete">
-
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
     </form>
-    <?php } ?>
-    <h2>
-      <?php
-      print_title();
-      ?>
-    </h2>
-    <?php
-    print_description();
-    ?>
   </body>
 </html>

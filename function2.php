3bf6fd67a18432897b9c0884bf2167524b2ad129
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2021-07-16 09:52:20 +0900

    php 함수

---
+++ function2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>function</title>
+  </head>
+  <body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+    <?php
+      function basic(){
+        print("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>");
+      }
+      basic();
+      echo "<br>";
+      basic();
+      echo "<br>";
+    ?>
+    <h2>Basic2</h2>
+    <?php
+      basic();
+    ?>
+  </body>
+</html>
